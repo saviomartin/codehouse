0b6f30c61c33ddadfca203c8328fce4ffbba76aa
saviomartin <saviomartin2007@gmail.com>
2021-06-03 19:13:59 +0530

    landing page progress

---
+++ components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="h-full bg-image min-h-screen w-full text-[#ECF2F5] flex items-center justify-center">
+      <div className="w-6/12 h-full flex items-center justify-center flex-col">
+        <h1 className="font-bold text-[6rem]">
+          Worl'd biggest storehouse of developer cheetsheets
+        </h1>
+      </div>
+      <div className="w-6/12 h-full flex items-center justify-center flex-col"></div>
+    </div>
+  );
+};
+
+export default Hero;
