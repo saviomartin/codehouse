c06c8f19c5fec8456abf3025e8f1f85f479bc380
saviomartin <saviomartin2007@gmail.com>
2021-06-04 15:14:39 +0530

    list view complete

--- components/Item.js
+++ components/Item.js
@@ -11,14 +11,14 @@ import {
 const Item = ({ data, list = true }) => {
   const { cheetsheet_name, website_url, category, twitter_handle } = data;
   return list ? (
-    <div className="flex items-center justify-center p-3 rounded-md duration-500 white-light-shadow bg-white m-2 w-10/12 border border-[#ddd] hover:border-[#3d5eff98] item-hover-text">
-      <div className="w-3/12 relative h-full">
+    <div className="flex items-center p-4 rounded-md duration-500 white-light-shadow bg-white m-2 w-10/12 border border-[#ddd] hover:border-[#3d5eff98] item-hover-text">
+      <div className="w-[250px] relative h-full">
         <Link href="/new">
           <a>
             <img
               src="https://websitesetup.org/wp-content/uploads/2021/01/CSS-Cheat-Sheet.jpg"
               alt=""
-              width="300"
+              width="250"
               className="rounded-md w-full"
             />
           </a>
@@ -29,7 +29,35 @@ const Item = ({ data, list = true }) => {
           </div>
         </Btn>
       </div>
-      <div className="w-9/12 h-full"></div>
+      <div className="w-9/12 h-full px-3 py-5 flex items-start justify-between flex-col">
+        <Link href="/new">
+          <a>
+            <h1 className="font-bold text-xl duration-500 hover:text-[#3d5eff]">
+              {cheetsheet_name}
+            </h1>
+            <p className="text-[12px] text-[#666] mt-1">{cheetsheet_name}</p>
+          </a>
+        </Link>
+        <div className="flex items-center justify-start mt-1 w-full">
+          <Btn className="rounded-md">
+            <div className="shine bg-[#3d5eff] text-white duration-500 px-4 py-2 text-sm capitalize rounded-lg font-semibold flex items-center justify-center menu-animation-hover poppins">
+              80
+              <FiTriangle className="text-sm ml-1 span duration-500" />
+            </div>
+          </Btn>
+          <Btn className="rounded-md ml-1">
+            <div className="border border-[#3d5eff] text-[#3d5eff] duration-500 px-4 py-2 text-sm capitalize rounded-lg font-semibold flex items-center justify-center menu-animation-hover poppins">
+              4
+              <FiMessageCircle className="text-sm ml-1 span duration-500" />
+            </div>
+          </Btn>
+          <Btn className="ml-1">
+            <div className="text-[#3d5eff] duration-500 px-2 py-3 h-full text-sm capitalize rounded-lg font-semibold flex items-center justify-center menu-animation-hover poppins">
+              <FiExternalLink className="text-sm span duration-500" />
+            </div>
+          </Btn>
+        </div>
+      </div>
     </div>
   ) : (
     <div className="flex justify-between flex-col p-5 rounded-md duration-500 white-light-shadow bg-white m-2 w-3/12 border border-[#ddd] hover:border-[#3d5eff98] item-hover-text">
