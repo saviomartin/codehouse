b18bbad21812db11605d598e88c071ef6d0b2d29
saviomartin <saviomartin2007@gmail.com>
2021-06-04 14:41:21 +0530

    fix err

--- components/Item.js
+++ components/Item.js
@@ -12,33 +12,35 @@ const Item = ({ data }) => {
   const { cheetsheet_name, website_url, category, twitter_handle } = data;
   return (
     <a className="flex justify-between flex-col p-5 rounded-md duration-500 white-light-shadow bg-white m-2 w-3/12 border border-[#ddd] hover:border-[#3d5eff98] item-hover-text">
-      <Link href="/post">
-        <a>
-          <div className="w-full relative">
-            <img
-              src="https://websitesetup.org/wp-content/uploads/2021/01/CSS-Cheat-Sheet.jpg"
-              alt=""
-              width="300"
-              className="rounded-md w-full mb-2"
-            />
-            <Btn className="rounded-md ml-1 absolute top-1 right-1">
-              <div className="bg-white p-2 text-[#F5BA31] duration-500 text-md capitalize rounded-md font-semibold flex items-center justify-center menu-animation-hover poppins">
-                <FiBookmark className="text-md span duration-500" />
-              </div>
-            </Btn>
-          </div>
-          <h1 className="font-bold text-lg duration-500 hover:text-[#3d5eff]">
-            {cheetsheet_name.length > 50
-              ? cheetsheet_name.slice(0, 50) + "..."
-              : cheetsheet_name}
-          </h1>
-          <p className="text-[12px] text-[#666] mt-1">
-            {cheetsheet_name.length > 100
-              ? cheetsheet_name.slice(0, 100) + "..."
-              : cheetsheet_name}
-          </p>
-        </a>
-      </Link>
+      <div className="">
+        <Link href="/new">
+          <a>
+            <div className="w-full relative">
+              <img
+                src="https://websitesetup.org/wp-content/uploads/2021/01/CSS-Cheat-Sheet.jpg"
+                alt=""
+                width="300"
+                className="rounded-md w-full mb-2"
+              />
+              <Btn className="rounded-md ml-1 absolute top-1 right-1">
+                <div className="bg-white p-2 text-[#F5BA31] duration-500 text-md capitalize rounded-md font-semibold flex items-center justify-center menu-animation-hover poppins">
+                  <FiBookmark className="text-md span duration-500" />
+                </div>
+              </Btn>
+            </div>
+            <h1 className="font-bold text-lg duration-500 hover:text-[#3d5eff]">
+              {cheetsheet_name.length > 50
+                ? cheetsheet_name.slice(0, 50) + "..."
+                : cheetsheet_name}
+            </h1>
+            <p className="text-[12px] text-[#666] mt-1">
+              {cheetsheet_name.length > 100
+                ? cheetsheet_name.slice(0, 100) + "..."
+                : cheetsheet_name}
+            </p>
+          </a>
+        </Link>
+      </div>
       <div className="flex items-center justify-start mt-1 w-full">
         <Btn className="rounded-md">
           <div className="shine bg-[#3d5eff] text-white duration-500 px-4 py-2 text-md capitalize rounded-lg font-semibold flex items-center justify-center menu-animation-hover poppins">
