56f73a88789cf3a9443f0a0e4f1a6128bea30b86
saviomartin <saviomartin2007@gmail.com>
2021-06-04 11:29:27 +0530

    add menu animation

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -6,10 +6,10 @@ const AppHeader = () => {
   return (
     <div className="w-full py-4 px-8 bg-white rounded-md white-light-shadow flex items-center justify-between">
       <div className="flex items-start justify-start w-5/12">
-        <Btn>
-          <div className="border border-[#ddd] hover:border-[#3d5eff] duration-500 px-5 py-[10px] text-lg capitalize rounded-lg font-semibold flex items-center justify-center">
+        <Btn shine={false}>
+          <div className="border border-[#ddd] hover:border-[#3d5eff] text-[#3d5eff] duration-500 px-5 py-[10px] text-lg capitalize rounded-lg font-semibold flex items-center justify-center menu-animation-hover">
             Menu
-            <FiMenu className="text-lg ml-1" />
+            <FiMenu className="text-lg ml-1 span" />
           </div>
         </Btn>
         <SearchBar />
--- components/Btn.js
+++ components/Btn.js
@@ -1,8 +1,10 @@
 import { Button } from "@material-ui/core";
 
-const Btn = ({ children }) => {
+const Btn = ({ children, shine = true }) => {
   return (
-    <Button className="!p-0 !h-auto !m-auto shine !block !min-w-0">
+    <Button
+      className={`!p-0 !h-auto !m-auto !block !min-w-0 ${shine && "shine"}`}
+    >
       {children}
     </Button>
   );
