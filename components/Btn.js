add4b0548d02848f71af4aa620b428d93d556539
saviomartin <saviomartin2007@gmail.com>
2021-06-04 11:30:45 +0530

    menu almost complete

--- components/Btn.js
+++ components/Btn.js
@@ -3,7 +3,9 @@ import { Button } from "@material-ui/core";
 const Btn = ({ children, shine = true }) => {
   return (
     <Button
-      className={`!p-0 !h-auto !m-auto !block !min-w-0 ${shine && "shine"}`}
+      className={`!p-0 !h-auto !m-auto !block !min-w-0 hover:!bg-transparent ${
+        shine && "shine"
+      }`}
     >
       {children}
     </Button>
