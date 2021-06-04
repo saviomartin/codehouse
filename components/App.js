03ff659e5ed63212d290e66cc0fe8ed5da5b6fcc
saviomartin <saviomartin2007@gmail.com>
2021-06-04 12:06:42 +0530

    created dummy data

--- components/App.js
+++ components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { AppHeader } from ".";
+import dummdata from "../dummydata.json";
 
 const App = () => {
   return (
     <div className="bg-[#ECF2F5] min-h-screen p-6">
       <AppHeader />
+      {JSON.stringify(dummdata)}
     </div>
   );
 };
