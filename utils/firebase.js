1dce286f2ba43bf9a1ae4f32d0519c559ecd51d3
saviomartin <saviomartin2007@gmail.com>
2021-06-06 11:49:32 +0530

    added config

--- utils/firebase.js
+++ utils/firebase.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBTH2-7uJKbjZ8slbPxaxny5MreXT-djhA",
+  authDomain: "codehouse-bysavio.firebaseapp.com",
+  projectId: "codehouse-bysavio",
+  storageBucket: "codehouse-bysavio.appspot.com",
+  messagingSenderId: "924657130605",
+  appId: "1:924657130605:web:2004acd9d22ab04d117364",
+  measurementId: "G-X6FCVKT31X",
 };
 
 firebase.initializeApp(firebaseConfig);
