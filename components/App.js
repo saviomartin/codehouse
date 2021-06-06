4d69255dd800d07c5d6ca04930eeb97147bfb35b
saviomartin <saviomartin2007@gmail.com>
2021-06-06 11:06:27 +0530

    add image loading

--- components/App.js
+++ components/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { AppHeader } from ".";
+import { AppHeader, Btn } from ".";
 import dummdata from "../dummydata.json";
 import { Item } from ".";
 import { harperFetch } from "../utils/HarperFetch";
 
 const App = (props) => {
   const [data, setData] = useState([]);
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(15);
 
   useEffect(async () => {
     const cheatSheets = await harperFetch({
@@ -24,6 +24,16 @@ const App = (props) => {
         {data.slice(0, count).map((cheetsheet, key) => (
           <Item data={cheetsheet} key={key} {...props} />
         ))}
+      </div>
+      <div className="w-full flex item-center justify-center mt-8">
+        <Btn>
+          <button
+            className="bg-app-gradient border border-[#391637] text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline shine"
+            onClick={() => setCount(count + 20)}
+          >
+            Load More ...
+          </button>
+        </Btn>
       </div>
     </div>
   );
