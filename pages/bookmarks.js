c9deffcc7143e111668516339a62ce219f80af96
saviomartin <saviomartin2007@gmail.com>
2021-06-08 11:22:21 +0530

    bookmarks page almost completed

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -6,15 +6,24 @@ const bookmarksPage = ({ bookmarks, user, fetchBookmarks }) => {
     <div className="bg-image h-full w-full overflow-visible">
       <Header user={user} />
       <div className="w-full h-full min-h-[90vh] rounded-md white-light-shadow flex justify-center items-center flex-wrap pt-10">
-        {bookmarks &&
-          bookmarks.map((cheetsheet, key) => (
-            <BookMarkItem
-              data={cheetsheet}
-              key={key}
-              bookmarks={bookmarks}
-              fetchBookmarks={fetchBookmarks}
-            />
-          ))}
+        {bookmarks
+          ? bookmarks.map((cheetsheet, key) => (
+              <BookMarkItem
+                data={cheetsheet}
+                key={key}
+                bookmarks={bookmarks}
+                fetchBookmarks={fetchBookmarks}
+              />
+            ))
+          : null}
+        {bookmarks.length < 1 && (
+          <div className="w-full flex items-center flex-col -mt-5">
+            <img src="/assets/svg/no-results-white.svg" className="h-[300px]" />
+            <h1 className="font-bold text-3xl text-white">
+              No BookMarks Found
+            </h1>
+          </div>
+        )}
       </div>
     </div>
   );
