c525bfdd74478dba546b24b9e79e39a10cc54eb0
saviomartin <saviomartin2007@gmail.com>
2021-06-08 14:09:10 +0530

    getting current post

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,41 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { harperFetch } from "../../utils/HarperFetch";
 
 const Cheatsheet = () => {
   const router = useRouter();
 
+  const [data, setData] = useState([]);
   const { id } = router.query;
-  return <div>hi {id}</div>;
+
+  const filterPosts = (data, id) => {
+    if (!id) {
+      return data;
+    }
+
+    return data.filter((cheatsheet) => {
+      const cheatId = cheatsheet.id;
+      return cheatId.includes(id);
+    });
+  };
+
+  useEffect(async () => {
+    const cheatSheets = await harperFetch({
+      operation: "sql",
+      sql: "SELECT * FROM dev.cheatsheets",
+    });
+
+    setData(cheatSheets);
+  }, []);
+
+  // gets the currentPost
+  const currentPost = filterPosts(data, id);
+
+  return (
+    <div className="bg-image h-screen text-white">
+      hi {JSON.stringify(currentPost)}
+    </div>
+  );
 };
 
 export default Cheatsheet;
