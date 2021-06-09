82bd13911763ef364cecde10d3d4134afc72e21e
saviomartin <saviomartin2007@gmail.com>
2021-06-09 16:10:37 +0530

    optimised _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,57 @@
 import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
+
+// styles
 import "tailwindcss/tailwind.css"; // tailwind jit
-import { SignInPopup } from "../components";
 import "../styles/App.css"; // custom styles
+
+// toaster
+import { Toaster } from "react-hot-toast";
+
+// components
+import { SignInPopup } from "../components";
+
+// firebase auth
 import { auth } from "../utils/firebase";
+
+// Router from next
 import Router from "next/router";
+
+// showing progress using nprogress
 import NProgress from "nprogress";
-import useLocalStorage from "../utils/UseLocalStorage";
 
 function MyApp({ Component, pageProps }) {
+  // loading state
   const [loading, setLoading] = useState(false);
+
+  // views
   const [darkMode, setDarkMode] = useState(false);
   const [listView, setListView] = useState(false);
 
+  // handling sign in popup
   const [open, setOpen] = useState(false);
   const [user, setUser] = useState([]);
+
+  // handling bookmarks
   const [bookmarks, setBookmarks] = useState([]);
 
+  // fetching bookmarks
   const fetchBookmarks = () => {
     if (window !== undefined) {
       if (window.localStorage.getItem("bookmarks")) {
         setBookmarks(JSON.parse(window.localStorage.getItem("bookmarks")));
       } else {
+        // adding a blank [] if no bookmarks found
         window.localStorage.setItem("bookmarks", JSON.stringify([]));
       }
     }
   };
 
+  // useeffect to handle bookmarks
   useEffect(() => {
     fetchBookmarks();
   }, []);
 
+  // destructured props
   const props = {
     darkMode,
     setDarkMode,
@@ -44,12 +65,14 @@ function MyApp({ Component, pageProps }) {
     fetchBookmarks,
   };
 
+  // handling auth and storing user if found
   auth().onAuthStateChanged((user) => {
     if (user) {
       setUser(user);
     }
   });
 
+  // showing loading and progress
   Router.events.on("routeChangeStart", (url) => {
     setLoading(true);
     NProgress.start();
