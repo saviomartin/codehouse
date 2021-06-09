f0da628d80d5dfe8df2a84721929557953578d06
saviomartin <saviomartin2007@gmail.com>
2021-06-09 15:11:36 +0530

    changed view of comment

--- components/utils/Comment.js
+++ components/utils/Comment.js
@@ -4,15 +4,22 @@ import React from "react";
 const Comment = ({ comment }) => {
   return (
     <div className="rounded-md border border-[#ccc] p-2 flex items-center hover:bg-white duration-200 mt-2">
-      <img
-        src={`https://unavatar.vercel.app/${comment.email}`}
-        alt={comment.email}
-        className="h-[50px] w-[50px] rounded-md pixelated white-light-shadow"
-      />
+      <div className="">
+        <img
+          src={
+            comment.photoURL
+              ? comment.photoURL
+              : `https://unavatar.vercel.app/${comment.email}`
+          }
+          alt={comment.email}
+          className="h-[50px] w-[50px] rounded-md pixelated white-light-shadow"
+        />
+      </div>
       <div className="ml-2">
         <h2 className="font-semibold text-md text-[#222]">{comment.comment}</h2>
         <h4 className="text-xs font-semibold text-[#666] capitalize">
-          {formatRelative(comment.time, new Date())}
+          {formatRelative(comment.time, new Date())} •
+          {(comment.name && " ", comment.name)}
         </h4>
       </div>
     </div>
