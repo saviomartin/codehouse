a835c55bf854ea4158b542a0708ae3ad9dfeb4a5
saviomartin <saviomartin2007@gmail.com>
2021-06-09 15:55:58 +0530

    optimised comments

--- components/utils/Comment.js
+++ components/utils/Comment.js
@@ -1,20 +1,22 @@
-import { formatRelative } from "date-fns";
 import React from "react";
+
+// formating time
+// we get time in milliseconds
+// https://www.epochconverter.com/
+import { formatRelative } from "date-fns";
 
 const Comment = ({ comment }) => {
   return (
     <div className="rounded-md border border-[#ccc] p-2 flex items-center hover:bg-white duration-200 mt-2">
-      <div className="">
-        <img
-          src={
-            comment.photoURL
-              ? comment.photoURL
-              : `https://unavatar.vercel.app/${comment.email}`
-          }
-          alt={comment.email}
-          className="h-[50px] w-[50px] rounded-md pixelated white-light-shadow"
-        />
-      </div>
+      <img
+        src={
+          comment.photoURL
+            ? comment.photoURL
+            : `https://unavatar.vercel.app/${comment.email}`
+        }
+        alt={comment.email}
+        className="h-[50px] w-[50px] rounded-md pixelated white-light-shadow"
+      />
       <div className="ml-2">
         <h2 className="font-semibold text-md text-[#222]">{comment.comment}</h2>
         <h4 className="text-xs font-semibold text-[#666] capitalize">
