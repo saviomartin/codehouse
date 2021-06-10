1ed2f2b6a975218e1351fb19a6e296dd8c9184a8
saviomartin <saviomartin2007@gmail.com>
2021-06-10 16:37:07 +0530

    created up search bar

--- components/core/Sidebar.js
+++ components/core/Sidebar.js
@@ -11,12 +11,25 @@ const Sidebar = ({ showDrawer, toggleDrawer }) => {
 
   return (
     <Drawer anchor="left" open={showDrawer} onClose={toggleDrawer}>
-      <div className="w-auto h-full flex items-center justify-between flex-col">
-        <div className="h-full w-full flex items-center flex-col bg-black"></div>
-        <Tabs value={value} onChange={handleChange}>
+      <div className="w-auto h-full flex items-center justify-between flex-col bg-[#ECF2F5]">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          className="bg-white border border-t-[#ccc] white-light-shadow"
+        >
           <Tab label="Categories" value="categories" />
           <Tab label="Sources" value="sources" />
         </Tabs>
+        <div className="h-full w-full flex items-center flex-col py-3">
+          <input
+            type="text"
+            className="w-[92.5%] h-10 border border-[#888] hover:border-[#4469FA] focus:border-[#4469FA] rounded-md px-3"
+            placeholder={`Search ${
+              value.charAt(0).toUpperCase() + value.slice(1)
+            }`}
+          />
+          {JSON.stringify(value)}
+        </div>
       </div>
     </Drawer>
   );
