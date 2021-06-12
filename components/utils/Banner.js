ceaa5adca433e75a88c50700408c51d6809c3f3a
saviomartin <saviomartin2007@gmail.com>
2021-06-12 14:32:40 +0530

    create sources page

--- components/utils/Banner.js
+++ components/utils/Banner.js
@@ -24,12 +24,12 @@ const Banner = ({ text, website_url, image_url }) => {
   // intent link for reddit
   const redditLink = `http://www.reddit.com/submit?url=${website_url}&title=${text}`;
 
-  return (
+  return image_url ? (
     <div className="w-full px-3 py-6 my-2 relative flex items-center justify-center">
       <img
         src={image_url}
         alt={text}
-        className="h-[160px] max-w-[260px] rounded-md"
+        className="h-[160px] max-w-[260px] rounded-md pixelated"
       />
       <div className="block ml-5">
         <h1 className="text-4xl font-bold capitalize text-[#222]">
@@ -39,6 +39,55 @@ const Banner = ({ text, website_url, image_url }) => {
           Curated list of {text} cheatsheets! Enjoy 🥳
         </p>
         <div className="flex mt-3">
+          <a href={twitterLink} rel="noreferrer" target="_blank">
+            <Btn className="!p-0 !w-auto !h-auto !m-0">
+              <button className="bg-[#1DA1F2] text-white font-semibold py-2 px-3 rounded focus:outline-none focus:shadow-outline shine flex items-center text-sm">
+                Twitter
+                <FaTwitter className="text-lg ml-1" />
+              </button>
+            </Btn>
+          </a>
+          <a href={redditLink} rel="noreferrer" target="_blank">
+            <Btn className="!p-0 !w-auto !h-auto !m-0 !ml-1">
+              <button className="bg-[#F34423] text-white font-semibold py-2 px-3 rounded focus:outline-none focus:shadow-outline shine flex items-center text-sm">
+                Reddit
+                <FaRedditAlien className="text-lg ml-1 -mt-1" />
+              </button>
+            </Btn>
+          </a>
+          <a href={facebookLink} rel="noreferrer" target="_blank">
+            <Btn className="!p-0 !w-auto !h-auto !m-0 !ml-1">
+              <button className="bg-[#3F5793] text-white font-semibold py-2 px-3 rounded focus:outline-none focus:shadow-outline shine flex items-center text-sm">
+                Facebook
+                <FaFacebookSquare className="text-lg ml-1" />
+              </button>
+            </Btn>
+          </a>
+          <a href={linkedInLink} rel="noreferrer" target="_blank">
+            <Btn className="!p-0 !w-auto !h-auto !m-0 !ml-1">
+              <button className="bg-[#0375B4] text-white font-semibold py-2 px-3 rounded focus:outline-none focus:shadow-outline shine flex items-center text-sm">
+                LinkedIn
+                <FaLinkedin className="text-lg ml-1" />
+              </button>
+            </Btn>
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="w-full py-6 my-2 relative flex items-center justify-center">
+      <div className="block ml-5 text-center">
+        <a
+          className="text-4xl font-bold text-blue-600 continuous-line"
+          href={`https://${text}`}
+          target="_blank"
+        >
+          {text}
+        </a>
+        <p className="text-[#666] mt-3">
+          Curated list of cheatsheets from {text}! Enjoy 🥳
+        </p>
+        <div className="flex mt-3 items-center justify-center">
           <a href={twitterLink} rel="noreferrer" target="_blank">
             <Btn className="!p-0 !w-auto !h-auto !m-0">
               <button className="bg-[#1DA1F2] text-white font-semibold py-2 px-3 rounded focus:outline-none focus:shadow-outline shine flex items-center text-sm">
---
+++ pages/source/[name].js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from "react";
+
+// use router for params
+import { useRouter } from "next/router";
+
+// components
+import { AppHeader, Banner, Item } from "../../components";
+
+// fetching data
+import { harperFetch } from "../../utils/HarperFetch";
+
+// for inifinite scroll
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const name = (props) => {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(6); // count of posts that should load first
+
+  const router = useRouter();
+
+  const { name } = router.query;
+
+  // filters
+  const [searchTerm, setSearchTerm] = useState(""); // search
+  const [sort, setSort] = useState("popular"); // sort
+
+  useEffect(async () => {
+    setData([]);
+
+    // fetching
+    const cheatSheets = await harperFetch({
+      operation: "sql",
+      sql: "SELECT * FROM dev.cheatsheets",
+    });
+
+    // sorting
+    if (sort === "newest") {
+      cheatSheets
+        .sort((a, b) => {
+          return a.__createdtime__ - b.__createdtime__;
+        })
+        .reverse();
+    } else if (sort === "oldest") {
+      cheatSheets.sort((a, b) => {
+        return a.__createdtime__ - b.__createdtime__;
+      });
+    } else {
+      cheatSheets.sort((a, b) => {
+        if (a.upvotes.length > b.upvotes.length) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    }
+
+    // data to be used
+    await setData(cheatSheets);
+  }, [sort]);
+
+  // destructuring props
+  const { user, setOpen } = props;
+
+  // filtering posts (search)
+  const filterPosts = (data, query) => {
+    if (!query) {
+      return data;
+    }
+
+    return data.filter((cheatsheet) => {
+      const cheatsheetName = cheatsheet.cheatsheet_name.toLowerCase();
+      return cheatsheetName.includes(query.toLowerCase());
+    });
+  };
+
+  // all posts gets stored here
+  const filteredPosts = filterPosts(data, searchTerm);
+
+  return (
+    <div className="bg-[#ECF2F5] min-h-screen p-6">
+      <AppHeader
+        {...props}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
+        sort={sort}
+        setSort={setSort}
+      />
+      <Banner
+        text={name}
+        website_url={`https://codehouse.vercel.app/source/${name}`}
+      />
+      <InfiniteScroll
+        dataLength={count} //This is important field to render the next data
+        next={() => setCount(count + 5)}
+        hasMore={count >= filteredPosts.length ? false : true}
+      >
+        <div className="flex justify-center mt-5 w-full flex-wrap">
+          {filteredPosts
+            .filter((cheatsheet) => {
+              const url = new URL(cheatsheet.website_url);
+
+              const cheatsheetName = url.hostname.toLowerCase();
+              return cheatsheetName.includes(name && name.toLowerCase());
+            })
+            .slice(0, count)
+            .map((cheetsheet, key) => (
+              <Item
+                data={cheetsheet}
+                key={key}
+                {...props}
+                setOpen={setOpen}
+                user={user}
+              />
+            ))}
+        </div>
+      </InfiniteScroll>
+
+      {data.length > 1 && filteredPosts.length < 1 && (
+        <div className="w-full flex items-center flex-col">
+          <img src="/assets/svg/no-results.svg" className="h-[300px]" />
+          <h1 className="font-bold text-3xl">No Results Found</h1>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default name;
