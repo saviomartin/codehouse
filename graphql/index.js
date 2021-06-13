09507fb7c4fe5afe657f25d6f16ee06e6fbb4a0f
saviomartin <saviomartin2007@gmail.com>
2021-06-13 12:38:19 +0530

    hope this will fix err

--- graphql/index.js
+++ graphql/index.js
@@ -48,6 +48,6 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server
-  .listen({ port: process.env.PORT || 5000 })
-  .then(({ url }) => console.log(`Server ready at ${url}`));
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
