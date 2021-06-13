673deed8ce4adffe10e01361306b53902f605535
saviomartin <saviomartin2007@gmail.com>
2021-06-13 18:46:23 +0530

    created api for popular

---
+++ pages/api/cheatsheets/popular.js
@@ -0,0 +1,38 @@
+export default async (req, res) => {
+  const request = await fetch(process.env.NEXT_PUBLIC_DB_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${process.env.NEXT_PUBLIC_DB_AUTHORIZATION}`,
+    },
+    body: JSON.stringify({
+      operation: "sql",
+      sql: "SELECT * FROM dev.cheatsheets",
+    }),
+  });
+
+  const data = await request.json();
+
+  const result = data.map((item) => {
+    return {
+      id: item.id,
+      cheatsheet_name: item.cheatsheet_name,
+      website_url: item.website_url,
+      category: item.category,
+      twitter_handle: item.twitter_handle,
+      comments: item.comments.length,
+      upvotes: item.upvotes.length,
+      addedby: item.addedby,
+    };
+  });
+
+  res.status(200).json(
+    result.sort((a, b) => {
+      if (a.upvotes.length > b.upvotes.length) {
+        return -1;
+      } else {
+        return 1;
+      }
+    })
+  );
+};
