ac7e8a84403e568db630ef241c0359c27bb798dc
saviomartin <saviomartin2007@gmail.com>
2021-06-13 15:19:34 +0530

    tying again

--- graphql/index.js
+++ graphql/index.js
@@ -46,6 +46,8 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  introspection: true,
+  playground: true,
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
