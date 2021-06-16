2035c6ca286c23a8c00733b501cb029c722f0888
saviomartin <saviomartin2007@gmail.com>
2021-06-16 16:25:33 +0530

    creating contributors page

---
+++ pages/contributors.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Header, SvgBanner } from "../components";
+
+const contributors = ({ darkMode, user }) => {
+  return (
+    <div className="bg-image h-full w-full overflow-visible min-h-screen">
+      <Header user={user} />
+      <div className="flex w-full items-center justify-center mt-3 mb-2 flex-col lg:flex-row">
+        <img
+          src="/assets/svg/contributors-white.svg"
+          className="h-[230px] lg:h-[300px]"
+        />
+
+        <div className="w-full lg:w-5/12 overflow-hidden">
+          <h1 className="text-2xl lg:text-4xl font-bold text-[#fafafa] text-center lg:text-left">
+            Your BookMarks
+          </h1>
+          <p className="text-xs lg:text-sm text-[#ccc] text-center lg:text-left">
+            These are the cheatsheets you have bookmarked for later! ✌️
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default contributors;
