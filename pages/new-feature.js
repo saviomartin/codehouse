2f9670fe671a2e962af6a5f25216da8dfee594bc
saviomartin <saviomartin2007@gmail.com>
2021-06-18 08:32:32 +0530

    checking if radio works

--- pages/new-feature.js
+++ pages/new-feature.js
@@ -60,22 +60,18 @@ const NewFeature = ({ user }) => {
               placeholder="description"
             />
           </div>
-          <div className="mb-2">
-            <RadioGroup value={type} onChange={handleChange("type")}>
-              <div className="flex radio-grp">
-                <FormControlLabel
-                  value="feature-request"
-                  control={<Radio />}
-                  label="Feature Request"
-                />
-                <FormControlLabel
-                  value="bug-report"
-                  control={<Radio />}
-                  label="Bug Report"
-                />
-              </div>
-            </RadioGroup>
-          </div>
+          <RadioGroup value={type} onChange={handleChange("type")}>
+            <FormControlLabel
+              value="feature-request"
+              control={<Radio />}
+              label="Feature Request"
+            />
+            <FormControlLabel
+              value="bug-report"
+              control={<Radio />}
+              label="Bug Report"
+            />
+          </RadioGroup>
 
           <div className="flex items-center justify-between">
             <Btn>
@@ -97,9 +93,9 @@ const NewFeature = ({ user }) => {
                   "Anonymous"
                 )}
               </h3>
-              <span class="flex h-3 w-3 relative ml-2">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+              <span className="flex h-3 w-3 relative ml-2">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
               </span>
             </div>
           </div>
