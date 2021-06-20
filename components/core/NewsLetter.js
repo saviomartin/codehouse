81e95130023ed63de4c74428acf2fe32dcfb6a47
saviomartin <saviomartin2007@gmail.com>
2021-06-20 16:36:56 +0530

    created ui

--- components/core/NewsLetter.js
+++ components/core/NewsLetter.js
@@ -4,6 +4,13 @@ import { FiSend } from "react-icons/fi";
 const NewsLetter = () => {
   const [email, setEmail] = useState("");
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // logic
+    // TODO:
+  };
+
   return (
     <div className="bg-image h-auto w-full">
       <div className="h-auto min-h-[45vh] w-full rounded-md bg-[rgba(0,0,0,0.6)] flex items-center justify-center flex-wrap flex-col">
@@ -16,21 +23,27 @@ const NewsLetter = () => {
         </p>
         <div className="w-11/12 lg:w-6/12 xl:w-6/12">
           <div className="w-full p-[1px] bg-app-gradient-2 mt-4 rounded-md">
-            <div className="bg-[#0A0719] flex duration-500 focus:border-[#3d5eff] pl-3 rounded-lg p-1 w-full items-center justify-between">
+            <form
+              className="bg-[#0A0719] flex duration-500 focus:border-[#3d5eff] pl-3 rounded-lg p-1 w-full items-center justify-between"
+              onSubmit={onSubmit}
+            >
               <input
                 type="text"
                 placeholder="johndoe@gmail.com"
-                className="h-full py-1 pl-1 w-full bg-transparent dark:text-white text-sm lg:text-base"
+                className="h-full py-1 pl-1 w-full bg-transparent text-white text-sm lg:text-base"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-              <div className="bg-[#764dff] p-2 lg:p-3 cursor-pointer shine rounded-lg text-sm lg:text-base">
+              <div
+                className="bg-[#764dff] p-2 lg:p-3 cursor-pointer shine rounded-lg text-sm lg:text-base"
+                onClick={onSubmit}
+              >
                 <FiSend
                   className="text-white -ml-1"
                   style={{ transform: "rotate(45deg)" }}
                 />
               </div>
-            </div>
+            </form>
           </div>
           <h3 className="bg-text-gradient font-semibold mb-2 uppercase mt-2 tracking-wider text-sm lg:text-base xl:text-base">
             PS: We won't spam
