ed91ebf0f7a057f4098aaed98e8860b68daa8aeb
saviomartin <saviomartin2007@gmail.com>
2021-06-20 12:57:33 +0530

    features page responsive

--- components/core/Hero.js
+++ components/core/Hero.js
@@ -39,57 +39,57 @@ const Hero = ({ setOpen, user, setUser }) => {
       name: "300+ Cheatsheets",
       description:
         "Code house is super huge enough and have more than 300+ cheatsheets",
-      icon: <FiPackage className="text-4xl" />,
+      icon: <FiPackage className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Category Filter",
       description:
         "Feel free to filter using categories, it can help you find the best one.",
-      icon: <FiLayers className="text-4xl" />,
+      icon: <FiLayers className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Filter By Source",
       description:
         "You can filter cheatsheets by source, eg: medium.com or overapi.com",
-      icon: <FiRss className="text-4xl" />,
+      icon: <FiRss className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Bookmark Favorites",
       description: "You can bookmark cheatsheets for you to have a look later.",
-      icon: <FiBookmark className="text-4xl" />,
+      icon: <FiBookmark className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Request Features",
       description:
         "You can help us become code house better by adding your feature requests.",
-      icon: <FiCloudLightning className="text-4xl" />,
+      icon: <FiCloudLightning className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Contributors Page",
       description:
         "We proudly show our contributors, there is a dedicated contributors page.",
-      icon: <FiUsers className="text-4xl" />,
+      icon: <FiUsers className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Add new Cheatsheet",
       description:
         "You can add a new cheatsheet to code house, without even leaving the browser",
-      icon: <FiFolderPlus className="text-4xl" />,
+      icon: <FiFolderPlus className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Report Cheatsheet",
       description: "You can report a cheatsheet if you find it suspicious. ",
-      icon: <FiFlag className="text-4xl" />,
+      icon: <FiFlag className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Fully Responsive",
       description: "Fully responsive, you can use it on any devices.",
-      icon: <FiSmartphone className="text-4xl" />,
+      icon: <FiSmartphone className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
     {
       name: "Dark Mode",
       description: "Coders love dark mode, so you can enjoy it here too.",
-      icon: <FiMoon className="text-4xl" />,
+      icon: <FiMoon className="text-2xl lg:text-4xl xl:text-4xl" />,
     },
   ];
 
@@ -155,8 +155,8 @@ const Hero = ({ setOpen, user, setUser }) => {
           </div>
         </div>
       </div>
-      <div className="h-screen w-full flex items-center justify-center p-7">
-        <div className="p-[1px] h-full w-full rounded-md bg-[rgba(0,0,0,0.6)] flex items-center justify-center flex-wrap border border-[#B84F90]">
+      <div className="h-auto w-full flex items-center justify-center p-7">
+        <div className="lg:h-[80vh] xl:h-[80vh] w-full rounded-md bg-[rgba(0,0,0,0.6)] flex items-center justify-center flex-wrap border border-[#B84F90]">
           <div className="flex flex-wrap w-full justify-center">
             {features.map((feature, key) => (
               <FeatureComponent
