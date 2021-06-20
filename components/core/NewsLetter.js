74d6e9a864b4b14fc303760a922c03fc027fbe1f
saviomartin <saviomartin2007@gmail.com>
2021-06-20 15:05:49 +0530

    newsletter service responsive

--- components/core/NewsLetter.js
+++ components/core/NewsLetter.js
@@ -7,14 +7,14 @@ const NewsLetter = () => {
   return (
     <div className="bg-image h-auto w-full">
       <div className="h-auto min-h-[45vh] w-full rounded-md bg-[rgba(0,0,0,0.6)] flex items-center justify-center flex-wrap flex-col">
-        <h1 className="text-[#ECF2F5] text-4xl font-bold">
+        <h1 className="text-[#ECF2F5] text-2xl lg:text-4xl xl:text-4xl font-bold">
           Join Our NewsLetter
         </h1>
-        <p className="text-sm text-[#aaa]">
+        <p className="text-xs lg:text-sm xl:text-sm text-[#aaa] text-center lg:text-left xl:text-left">
           Never miss any new amazing cheatsheets, get them right onto your
           mails!
         </p>
-        <div className="w-6/12">
+        <div className="w-11/12 lg:w-6/12 xl:w-6/12">
           <div className="w-full p-[1px] bg-app-gradient-2 mt-4 rounded-md">
             <div className="bg-[#0A0719] flex duration-500 focus:border-[#3d5eff] pl-3 rounded-lg p-1 w-full items-center justify-between">
               <input
@@ -32,7 +32,7 @@ const NewsLetter = () => {
               </div>
             </div>
           </div>
-          <h3 className="bg-text-gradient font-semibold mb-2 uppercase mt-2 tracking-wider">
+          <h3 className="bg-text-gradient font-semibold mb-2 uppercase mt-2 tracking-wider text-sm lg:text-base xl:text-base">
             PS: We won't spam
           </h3>
         </div>
