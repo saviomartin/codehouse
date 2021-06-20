f32f2a27e21b5b48026e66319f190ae30641a094
saviomartin <saviomartin2007@gmail.com>
2021-06-20 12:47:42 +0530

    features page complete

--- components/core/Hero.js
+++ components/core/Hero.js
@@ -5,7 +5,19 @@ import { Button } from "@material-ui/core";
 
 // icons
 import { BsLightning } from "react-icons/bs";
-import { FiGithub, FiLayers } from "react-icons/fi";
+import {
+  FiBookmark,
+  FiCloudLightning,
+  FiFlag,
+  FiFolderPlus,
+  FiGithub,
+  FiLayers,
+  FiMoon,
+  FiPackage,
+  FiRss,
+  FiSmartphone,
+  FiUsers,
+} from "react-icons/fi";
 
 // link
 import Link from "next/link";
@@ -24,10 +36,60 @@ const Hero = ({ setOpen, user, setUser }) => {
 
   const features = [
     {
+      name: "300+ Cheatsheets",
+      description:
+        "Code house is super huge enough and have more than 300+ cheatsheets",
+      icon: <FiPackage className="text-4xl" />,
+    },
+    {
       name: "Category Filter",
       description:
         "Feel free to filter using categories, it can help you find the best one.",
       icon: <FiLayers className="text-4xl" />,
+    },
+    {
+      name: "Filter By Source",
+      description:
+        "You can filter cheatsheets by source, eg: medium.com or overapi.com",
+      icon: <FiRss className="text-4xl" />,
+    },
+    {
+      name: "Bookmark Favorites",
+      description: "You can bookmark cheatsheets for you to have a look later.",
+      icon: <FiBookmark className="text-4xl" />,
+    },
+    {
+      name: "Request Features",
+      description:
+        "You can help us become code house better by adding your feature requests.",
+      icon: <FiCloudLightning className="text-4xl" />,
+    },
+    {
+      name: "Contributors Page",
+      description:
+        "We proudly show our contributors, there is a dedicated contributors page.",
+      icon: <FiUsers className="text-4xl" />,
+    },
+    {
+      name: "Add new Cheatsheet",
+      description:
+        "You can add a new cheatsheet to code house, without even leaving the browser",
+      icon: <FiFolderPlus className="text-4xl" />,
+    },
+    {
+      name: "Report Cheatsheet",
+      description: "You can report a cheatsheet if you find it suspicious. ",
+      icon: <FiFlag className="text-4xl" />,
+    },
+    {
+      name: "Fully Responsive",
+      description: "Fully responsive, you can use it on any devices.",
+      icon: <FiSmartphone className="text-4xl" />,
+    },
+    {
+      name: "Dark Mode",
+      description: "Coders love dark mode, so you can enjoy it here too.",
+      icon: <FiMoon className="text-4xl" />,
     },
   ];
 
@@ -93,16 +155,18 @@ const Hero = ({ setOpen, user, setUser }) => {
           </div>
         </div>
       </div>
-      <div className="h-screen w-full flex items-center justify-center p-10">
+      <div className="h-screen w-full flex items-center justify-center p-7">
         <div className="p-[1px] h-full w-full rounded-md bg-[rgba(0,0,0,0.6)] flex items-center justify-center flex-wrap border border-[#B84F90]">
-          {features.map((feature, key) => (
-            <FeatureComponent
-              name={feature.name}
-              description={feature.description}
-              icon={feature.icon}
-              key={key}
-            />
-          ))}
+          <div className="flex flex-wrap w-full justify-center">
+            {features.map((feature, key) => (
+              <FeatureComponent
+                name={feature.name}
+                description={feature.description}
+                icon={feature.icon}
+                key={key}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- components/utils/FeatureComponent.js
+++ components/utils/FeatureComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FeatureComponent = ({ name, description, icon }) => {
   return (
-    <div className="mx-1 relative flex items-center justify-center flex-col w-[20%] text-center overflow-hidden">
+    <div className="mx-4 my-6 relative flex items-center justify-start flex-col w-[17%] text-center overflow-hidden">
       <div className="p-5 rouned-bg">{icon}</div>
       <h3 className="font-bold text-xl mt-5">{name}</h3>
       <p className="text-[#ccc] text-sm">{description}</p>
