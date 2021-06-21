9890c48b948b553392754f85cc9453e4f11b6b39
saviomartin <saviomartin2007@gmail.com>
2021-06-21 10:18:49 +0530

    fix some errors

--- components/utils/Loader.js
+++ components/utils/Loader.js
@@ -3,10 +3,10 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="w-full flex items-center justify-center">
-      <div class="spinner">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
       </div>
     </div>
   );
--- pages/report-post.js
+++ pages/report-post.js
@@ -212,9 +212,9 @@ const reportPost = (props) => {
                     "Anonymous"
                   )}
                 </h3>
-                <span class="flex h-3 w-3 relative ml-2">
-                  <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                  <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                <span className="flex h-3 w-3 relative ml-2">
+                  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                  <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
                 </span>
               </div>
             </div>
