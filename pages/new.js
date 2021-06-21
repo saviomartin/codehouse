cd8578db40d7c03500fa4d6179d6042b6c601ffd
saviomartin <saviomartin2007@gmail.com>
2021-06-21 07:47:40 +0530

    fixing new

--- pages/new.js
+++ pages/new.js
@@ -10,14 +10,16 @@ import { v4 as uuidv4 } from "uuid";
 import toast from "react-hot-toast";
 
 // Btn
-import { Btn } from "../components";
+import { Btn, Header } from "../components";
 import { FiCheck, FiX } from "react-icons/fi";
 
 // material ui select
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 
-const New = ({ user }) => {
+const New = (props) => {
+  const { user } = props;
+
   // default values
   const [values, setValues] = useState({
     cheatsheet_name: "",
@@ -141,103 +143,106 @@ const New = ({ user }) => {
   ];
 
   return (
-    <div className="h-full min-h-screen text-[#ECF2F5] w-full bg-image p-3 flex items-center justify-center flex-col">
-      <h1 className="text-2xl md:text-4xl lg:text-4xl xl:text-4xl font-bold mb-1 lg:mb-3 xl:mb-3 text-center">
-        Create New Cheatsheet
-      </h1>
-      <div className="w-full lg:w-7/12 xl:w-7/12 h-full bg-white dark:bg-[#2f2f2f] rounded-xl m-1">
-        <form
-          className="bg-transparent rounded-xl h-full px-8 pt-6 pb-8 mb-4"
-          onSubmit={onSubmit}
-        >
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
-              Cheatsheet Name
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white"
-              value={cheatsheet_name}
-              onChange={handleChange("cheatsheet_name")}
-              type="text"
-              placeholder="Awesome React Cheatsheet"
-            />
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
-              Website URL
-            </label>
-            <div
-              className={`flex border-[1.75px] mb-3 rounded-md items-center dark:bg-[#1f1f1f] ${
-                is_url(website_url) ? "border-green-500" : "border-red-500"
-              }`}
-            >
+    <div className="w-full h-full bg-image">
+      <Header {...props} />
+      <div className="h-full min-h-screen text-[#ECF2F5] w-full p-3 flex items-center justify-center flex-col">
+        <h1 className="text-2xl md:text-4xl lg:text-4xl xl:text-4xl font-bold mb-1 lg:mb-3 xl:mb-3 text-center">
+          Create New Cheatsheet
+        </h1>
+        <div className="w-full lg:w-7/12 xl:w-7/12 h-full bg-white dark:bg-[#2f2f2f] rounded-xl m-1">
+          <form
+            className="bg-transparent rounded-xl h-full px-8 pt-6 pb-8 mb-4"
+            onSubmit={onSubmit}
+          >
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
+                Cheatsheet Name
+              </label>
               <input
-                className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:text-white bg-transparent"
-                value={website_url}
-                onChange={handleChange("website_url")}
-                placeholder="https://hashnode.com/"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white"
+                value={cheatsheet_name}
+                onChange={handleChange("cheatsheet_name")}
+                type="text"
+                placeholder="Awesome React Cheatsheet"
               />
-              {is_url(website_url) ? (
-                <FiCheck className="text-green-500 text-2xl mr-2" />
-              ) : (
-                <FiX className="text-red-500 text-xl mr-2" />
-              )}
             </div>
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
-              Category
-            </label>
-            <Select
-              value={category}
-              onChange={handleChange("category")}
-              className="shadow appearance-none border rounded w-full pr-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white flex justify-center pl-3 Epilogue-Imp"
-            >
-              {categories.map((category, key) => (
-                <MenuItem value={category} key={key}>
-                  {category}
-                </MenuItem>
-              ))}
-            </Select>
-          </div>
-          <div className="mb-2">
-            <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
-              Your Twitter Handle (optional)
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white"
-              value={twitter_handle}
-              onChange={handleChange("twitter_handle")}
-              placeholder="SavioMartin7"
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <Btn>
-              <button
-                className="bg-app-gradient border border-[#391637] text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline shine"
-                type="submit"
+            <div className="mb-2">
+              <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
+                Website URL
+              </label>
+              <div
+                className={`flex border-[1.75px] mb-3 rounded-md items-center dark:bg-[#1f1f1f] ${
+                  is_url(website_url) ? "border-green-500" : "border-red-500"
+                }`}
               >
-                Add New Cheatsheet
-              </button>
-            </Btn>
-            <div className="flex items-center">
-              <h3 className="text-green-400 font-medium">
-                Adding As{" "}
-                {user.email ? (
-                  <span className="font-bold">
-                    {user.displayName ? user.displayName : user.email}
-                  </span>
+                <input
+                  className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:text-white bg-transparent"
+                  value={website_url}
+                  onChange={handleChange("website_url")}
+                  placeholder="https://hashnode.com/"
+                />
+                {is_url(website_url) ? (
+                  <FiCheck className="text-green-500 text-2xl mr-2" />
                 ) : (
-                  "Anonymous"
+                  <FiX className="text-red-500 text-xl mr-2" />
                 )}
-              </h3>
-              <span class="flex h-3 w-3 relative ml-2">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-              </span>
+              </div>
             </div>
-          </div>
-        </form>
+            <div className="mb-6">
+              <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
+                Category
+              </label>
+              <Select
+                value={category}
+                onChange={handleChange("category")}
+                className="shadow appearance-none border rounded w-full pr-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white flex justify-center pl-3 Epilogue-Imp"
+              >
+                {categories.map((category, key) => (
+                  <MenuItem value={category} key={key}>
+                    {category}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
+            <div className="mb-2">
+              <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
+                Your Twitter Handle (optional)
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white"
+                value={twitter_handle}
+                onChange={handleChange("twitter_handle")}
+                placeholder="SavioMartin7"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <Btn>
+                <button
+                  className="bg-app-gradient border border-[#391637] text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline shine"
+                  type="submit"
+                >
+                  Add New Cheatsheet
+                </button>
+              </Btn>
+              <div className="flex items-center">
+                <h3 className="text-green-400 font-medium">
+                  Adding As{" "}
+                  {user.email ? (
+                    <span className="font-bold">
+                      {user.displayName ? user.displayName : user.email}
+                    </span>
+                  ) : (
+                    "Anonymous"
+                  )}
+                </h3>
+                <span class="flex h-3 w-3 relative ml-2">
+                  <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+                  <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
