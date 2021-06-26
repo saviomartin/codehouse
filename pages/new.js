ded9aa42ea0e8bdf0befb125768af27eba985a47
saviomartin <saviomartin2007@gmail.com>
2021-06-26 20:09:52 +0530

    FIX: typo

--- pages/new.js
+++ pages/new.js
@@ -224,7 +224,7 @@ const New = (props) => {
             </div>
             <div className="mb-2">
               <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-[#fafafa]">
-                Your Twitter Handle (optional)
+                Author Twitter Handle (optional)
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline dark:bg-[#1f1f1f] dark:border-[#555] dark:text-white"
