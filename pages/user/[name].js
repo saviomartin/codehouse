e48e5a9ccceea58cd379da75ed4c63f9f7118fa1
Jaagrav <jaagravseal03@gmail.com>
2021-07-26 14:08:42 +0530

    Getting username from URL using useRouter

--- pages/user/[name].js
+++ pages/user/[name].js
@@ -4,13 +4,18 @@ import React, {useEffect, useState} from 'react';
 import { RightBar, InfoBar, Header } from "../../components";
 
 import Head from "next/head";
+import { useRouter } from "next/router";
+
 export default function User(props) {
-    console.log(props);
+    const router = useRouter(); // router
+    const { name } = router.query;
+
+    console.log(name);
     return (
       <div className="bg-image">
         <Head>
           <title>
-            User - Code House
+            {name} - Code House
           </title>
         </Head>
         <Header {...props} />
